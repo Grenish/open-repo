7424268e9481c7464d33a52ad2860ac1486c3bc7
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-11-30 23:18:38 +0530

    Refactor tailwind.config.ts to extend colors and borderRadius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,40 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: 'var(--primary-100)',
-          200: 'var(--primary-200)',
-          300: 'var(--primary-300)',
-        },
-        accent: {
-          100: 'var(--accent-100)',
-          200: 'var(--accent-200)',
-        },
-        text: {
-          100: 'var(--text-100)',
-          200: 'var(--text-200)',
-        },
-        bg: {
-          100: 'var(--bg-100)',
-          200: 'var(--bg-200)',
-          300: 'var(--bg-300)',
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				'100': 'var(--primary-100)',
+  				'200': 'var(--primary-200)',
+  				'300': 'var(--primary-300)'
+  			},
+  			accent: {
+  				'100': 'var(--accent-100)',
+  				'200': 'var(--accent-200)'
+  			},
+  			text: {
+  				'100': 'var(--text-100)',
+  				'200': 'var(--text-200)'
+  			},
+  			bg: {
+  				'100': 'var(--bg-100)',
+  				'200': 'var(--bg-200)',
+  				'300': 'var(--bg-300)'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
