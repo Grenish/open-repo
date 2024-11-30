77b48a4139b3df1eca43c7c1390149401b72d8f5
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-11-30 23:20:12 +0530

    Refactor Repository interface to Repo interface in types/github.ts

--- types/github.ts
+++ types/github.ts
@@ -1,16 +1,16 @@
-export interface Repository {
-    id: number
-    name: string
-    full_name: string
-    description: string
-    language: string
-    stargazers_count: number
-    forks_count: number
-    open_issues_count: number
+export interface Repo {
+    id: number;
+    name: string;
+    full_name: string;
+    description: string | null;
+    language: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
     topics: string[]
-    html_url: string
+    html_url: string;
     owner: {
-        login: string
-        avatar_url: string
+        login: string;
+        avatar_url: string;
     }
 }
