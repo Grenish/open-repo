90198ba3ef96881dbfec836576cbe1957178482b
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-11-30 23:19:15 +0530

    Refactor RepoCard component to include flex layout and add link to GitHub

--- components/repo-card.tsx
+++ components/repo-card.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import { Star, GitFork, CircleDot, Github } from "lucide-react";
+import { Repo } from "@/types/github";
+import Link from "next/link";
 
-export default function RepoCard({ repo }: { repo: any }) {
+function FormatNumber({ number }: { number: number }) {
+  return number >= 1000 ? `${(number / 1000).toFixed(1)}k` : number.toString();
+}
+
+function ShortDescription({ description }: { description: string }) {
+  return description.length > 100
+    ? `${description.slice(0, 100)}...`
+    : description;
+}
+
+export default function RepoCard({ repo }: { repo: Repo }) {
   return (
-    <div className="p-4 border-2 rounded-xl relative overflow-hidden">
+    <div className="p-4 border-2 rounded-xl relative overflow-hidden flex flex-col">
       {/* Background image with overlay */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="relative w-full h-full">
@@ -20,43 +32,46 @@ export default function RepoCard({ repo }: { repo: any }) {
       </div>
 
       {/* Content */}
-      <div className="relative z-10">
-        <h2 className="text-primary-300 text-sm">{repo.owner.login}</h2>
-        <h2 className="text-primary-200 text-xl">{repo.name}</h2>
+      <div className="relative z-10 flex flex-col flex-1">
+        <div>
+          <h2 className="text-primary-300 text-sm">{repo.owner.login}</h2>
+          <h2 className="text-primary-200 text-xl">{repo.name}</h2>
 
-        <p className="text-text-200 text-sm mt-2">
-          {repo.description || "No description available."}
-        </p>
+          <p className="text-text-200 text-sm mt-2">
+            <ShortDescription description={repo.description || ""} />
+          </p>
 
-        {/* Stats */}
-        <div className="flex items-center gap-4 mt-4 flex-wrap">
-          <div className="inline-flex items-center gap-1">
-            <Star className="text-accent-200" size={16} />
-            <span className="text-text-200 text-xs">
-              {repo.stargazers_count}
-            </span>
-          </div>
-          <div className="inline-flex items-center gap-1">
-            <GitFork className="text-accent-200" size={16} />
-            <span className="text-text-200 text-xs">{repo.forks_count}</span>
-          </div>
-          <div className="inline-flex items-center gap-1">
-            <CircleDot className="text-accent-200" size={16} />
-            <span className="text-text-200 text-xs">
-              {repo.open_issues_count}
-            </span>
+          {/* Stats */}
+          <div className="flex items-center gap-4 mt-4 flex-wrap">
+            <div className="inline-flex items-center gap-1">
+              <Star className="text-accent-200" size={16} />
+              <span className="text-text-200 text-xs">
+                <FormatNumber number={repo.stargazers_count} />
+              </span>
+            </div>
+            <div className="inline-flex items-center gap-1">
+              <GitFork className="text-accent-200" size={16} />
+              <span className="text-text-200 text-xs">
+                <FormatNumber number={repo.forks_count} />
+              </span>
+            </div>
+            <div className="inline-flex items-center gap-1">
+              <CircleDot className="text-accent-200" size={16} />
+              <span className="text-text-200 text-xs">
+                <FormatNumber number={repo.open_issues_count} />
+              </span>
+            </div>
           </div>
         </div>
 
         {/* CTA buttons */}
-        <div className="mt-4 flex items-center justify-end gap-2">
-          <button
-            className="bg-bg-200 inline-flex items-center gap-2 text-text-200 text-xs p-2 px-4 rounded-lg"
-            onClick={() => window.open(repo.html_url, "_blank")}
-          >
-            <Github className="text-text-200" size={15} />
-            Open in GitHub
-          </button>
+        <div className="mt-auto flex items-center justify-end gap-2">
+          <Link href={repo.html_url} target="_blank">
+            <button className="bg-bg-200 inline-flex items-center gap-2 text-text-200 text-xs p-2 px-4 rounded-lg">
+              <Github className="text-text-200" size={15} />
+              Open in GitHub
+            </button>
+          </Link>
         </div>
       </div>
     </div>
