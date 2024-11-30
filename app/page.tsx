924b6ab019edd0e4e69f83248d79720f2e23b96d
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-11-30 23:19:02 +0530

    Refactor page component to include search functionality and lazy loading of repo grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
-import Image from "next/image";
+import { Suspense } from "react";
 import { FolderOpenDot } from "lucide-react";
-import RepoGrid from "@/components/repo-grid";
+import RepoGrid, { RepoGridSkeleton } from "@/components/repo-grid";
+import SearchBox from "@/components/search-box";
+import { SearchProvider } from "@/contexts/search-context";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-bg-100 px-4 py-6 md:px-6 lg:px-8">
-      <span className="inline-flex items-center gap-2">
-        <FolderOpenDot className="text-primary-200" size={54} />
-        <h2 className="text-primary-200 text-4xl">Open Repo</h2>
-      </span>
-      <h2 className="text-text-200 text-sm">
-        Discover the Open Source Project That Best Matches Your Skills and
-        Interests
-      </h2>
-      <div className="">
-        <RepoGrid />
-      </div>
-    </main>
+    <SearchProvider>
+      <main className="min-h-screen bg-bg-100 px-4 py-6 md:px-6 lg:px-8">
+        <span className="inline-flex items-center gap-2">
+          <FolderOpenDot className="text-primary-200" size={54} />
+          <h2 className="text-primary-200 text-4xl">Open Repo</h2>
+        </span>
+        <h2 className="text-text-200 text-sm">
+          Discover the Open Source Project That Best Matches Your Skills and
+          Interests
+        </h2>
+        <div className="">
+          <SearchBox />
+          <Suspense fallback={<RepoGridSkeleton />}>
+            <RepoGrid />
+          </Suspense>
+        </div>
+      </main>
+    </SearchProvider>
   );
 }
